57683b1f261dd02b51e1e0e97bb1e3eddc03bb61
Fine YUDU <fine_567@hotmail.com>
2024-12-30 18:33:55 -0600

    routes configuration done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { Page404Component } from '@shared/views/page404/page404.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        loadComponent:()=> import('./website/website.component'),
+        children:[
+            {
+                path:'about',
+                loadComponent:()=> import('./website/views/about/about.component')
+            },
+            { path:'', redirectTo:'about', pathMatch:'full' }
+        ]
+    },
+    {
+        path:'404',
+        component:Page404Component
+    },
+    { path:'', redirectTo:'', pathMatch:'full' },
+    { path:'**', redirectTo:'/404', pathMatch:'full' }
+];
